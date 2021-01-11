38fc54cb699af6c15e74103e1d69f9555830014f
0x018 <0x018@zxcv.icu>
2021-01-11 12:21:17 +0800

    js,c,关键字nc不屏蔽

--- example.js
+++ example.js
@@ -7,7 +7,10 @@ var text = [
 	`草,你大爷,这有一颗草`,
 	`卧槽!滚开,你怎么能这样`,
 	`生儿子煤屁眼`,
-	`坑爹啊坑爹啊小瘪三啊小瘪三啊小赤佬小赤佬哎哟妈呀小瘪三小瘪三`
+	`坑爹啊坑爹啊小瘪三啊小瘪三啊小赤佬小赤佬哎哟妈呀小瘪三小瘪三`,
+	`function (name){ console.log(name+" is nc !")}`,
+	`async function (a){ console.log(a)}`,
+	`#include <stdio.h>`,
 ];
 text.forEach((t, i) => {
 	console.log("输入:" + t);
